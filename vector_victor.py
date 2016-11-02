49e21dfd4bd87f5e73c104bf4878b277a4cfc178
John D. Bower <jdbower0525@gmail.com>
2016-11-02 16:26:35 -0400

    Finished basic

---
+++ vector_victor.py
@@ -0,0 +1,58 @@
+import math
+from nose.tools import raises
+
+class ShapeError(Exception):
+    pass
+
+def shape(vector_matrix):
+    rows = len(vector_matrix)
+    if type(vector_matrix[0]) == type([]):
+        columns = len(vector_matrix[0])
+        return (rows, columns)
+    else:
+        return (rows, )
+
+def compare_shapes(*args):
+    if len(set([shape(arg) for arg in args])) == 1:
+        return shape
+
+def vector_add(vector_one, vector_two):
+    if shape(vector_one) == shape(vector_two):
+        add_vectors = [v1 + v2 for v1,v2 in zip(vector_one, vector_two)]
+        return add_vectors
+    else:
+        raise ShapeError
+
+def vector_sub(vector_one, vector_two):
+    if shape(vector_one) == shape(vector_two):
+        sub_vectors = [v1 - v2 for v1,v2 in zip(vector_one, vector_two)]
+        return sub_vectors
+    else:
+        raise ShapeError
+
+def vector_sum(*args):
+    vector_shape = [shape(vector) for vector in args]
+    if min(vector_shape) == max(vector_shape):
+        vectors_summed = [sum(v1) for v1 in zip(*args)]
+        return vectors_summed
+    else:
+        raise ShapeError
+
+def dot(vector_one, vector_two):
+    if shape(vector_one) == shape(vector_two):
+        dot_vectors = sum([v1 * v2 for v1,v2 in zip(vector_one, vector_two)])
+        return dot_vectors
+    else:
+        raise ShapeError
+
+def vector_multiply(vector, scalar):
+    return [(number * scalar) for number in vector]
+
+def mean(*args):
+    return sum(*args)/len(*args)
+
+def vector_mean(*args):
+    return [mean(x) for x in zip(*args)]
+
+def magnitude(vector):
+    return ((sum([x**2 for x in vector])))**(1/2)
