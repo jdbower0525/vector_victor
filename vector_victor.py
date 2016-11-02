c2d0261f5740154d9b7aea8859fc3680997fc9ab
John D. Bower <jdbower0525@gmail.com>
2016-11-02 19:29:15 -0400

    setting up class test

--- vector_victor.py
+++ vector_victor.py
@@ -18,41 +18,42 @@ def compare_shapes(*args):
 
 def vector_add(vector_one, vector_two):
     if shape(vector_one) == shape(vector_two):
-        add_vectors = [v1 + v2 for v1,v2 in zip(vector_one, vector_two)]
-        return add_vectors
+        return [v1 + v2 for v1,v2 in zip(vector_one, vector_two)]
     else:
         raise ShapeError
 
 def vector_sub(vector_one, vector_two):
     if shape(vector_one) == shape(vector_two):
-        sub_vectors = [v1 - v2 for v1,v2 in zip(vector_one, vector_two)]
-        return sub_vectors
+        return [v1 - v2 for v1,v2 in zip(vector_one, vector_two)]
     else:
         raise ShapeError
 
 def vector_sum(*args):
     vector_shape = [shape(vector) for vector in args]
     if min(vector_shape) == max(vector_shape):
-        vectors_summed = [sum(v1) for v1 in zip(*args)]
-        return vectors_summed
+        return [sum(v1) for v1 in zip(*args)]
     else:
         raise ShapeError
 
 def dot(vector_one, vector_two):
     if shape(vector_one) == shape(vector_two):
-        dot_vectors = sum([v1 * v2 for v1,v2 in zip(vector_one, vector_two)])
-        return dot_vectors
+        return sum([v1 * v2 for v1,v2 in zip(vector_one, vector_two)])
     else:
         raise ShapeError
 
 def vector_multiply(vector, scalar):
     return [(number * scalar) for number in vector]
 
-def mean(*args):
-    return sum(*args)/len(*args)
-
 def vector_mean(*args):
     return [(sum(x)/len(args)) for x in zip(*args)]
 
 def magnitude(vector):
     return ((sum([x**2 for x in vector])))**(1/2)
+
+def matrix_row(matrix, row):
+    return matrix[row]
+
+def matrix_col(matrix, col):
+    return [row[col] for row in matrix]
+
+def matrix_add(matrix_1, matrix_2)
